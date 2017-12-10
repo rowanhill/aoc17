34b5a794a81aa68745f738ecc91fe61b8ace2e47
Rowan Hill <rowan.hill@softwire.com>
2017-12-10 10:22:13 +0000

    Tidy up day 10 a bit

--- day10/src/app.kt
+++ day10/src/app.kt
@@ -1,11 +1,5 @@
 fun main(args: Array<String>) {
-
-    var string = (0..255).toMutableList()
-
-    var skipSize = 0
-    var currentPosition = 0
-
-    fun reverseLength(length: Int) {
+    fun reverseLength(currentPosition: Int, length: Int, string: MutableList<Int>) {
         for (i in 0..((length-1)/2)) {
             val firstIndex = (currentPosition + i) % string.size
             val secondIndex = (currentPosition + length - 1 - i) % string.size
@@ -16,37 +10,50 @@ fun main(args: Array<String>) {
         }
     }
 
-    // part 1
-    val lengths = arrayOf(187,254,0,81,169,219,1,190,19,102,255,56,46,32,2,216)
+    fun knotHash(lengths: Array<Int>, string: MutableList<Int>, times: Int) {
+        var skipSize = 0
+        var currentPosition = 0
 
-    for (length in lengths) {
-        reverseLength(length)
-        currentPosition = (currentPosition + length + skipSize) % string.size
-        skipSize += 1
-    }
-    println(string[0] * string[1])
-
-    // part 2
-    val lengths2 = "187,254,0,81,169,219,1,190,19,102,255,56,46,32,2,216"
-            .map { it.toInt() }.toMutableList()
-    lengths2.addAll(arrayOf(17, 31, 73, 47, 23))
-
-    string = (0..255).toMutableList()
-    skipSize = 0
-    currentPosition = 0
-
-    (0 until 64).forEach {
-        for (length in lengths2) {
-            reverseLength(length)
-            currentPosition = (currentPosition + length + skipSize) % string.size
-            skipSize += 1
+        repeat(times) {
+            for (length in lengths) {
+                reverseLength(currentPosition, length, string)
+                currentPosition = (currentPosition + length + skipSize) % string.size
+                skipSize += 1
+            }
         }
     }
 
-    val hexes = string
-            .windowed(16, 16)
-            .map { it.reduce { a,b -> a.xor(b) } }
-            .joinToString("") { String.format("%1$02X",it) }
+    // part 1
+    run {
+        // prepare
+        val string = (0..255).toMutableList()
+        val lengths = arrayOf(187, 254, 0, 81, 169, 219, 1, 190, 19, 102, 255, 56, 46, 32, 2, 216)
 
-    println(hexes)
+        // process
+        knotHash(lengths, string, 1)
+
+        // print
+        println(string[0] * string[1])
+    }
+
+    // part 2
+    run {
+        // prepare
+        val string = (0..255).toMutableList()
+        val lengths = "187,254,0,81,169,219,1,190,19,102,255,56,46,32,2,216"
+                .map { it.toInt() }.toMutableList()
+        lengths.addAll(arrayOf(17, 31, 73, 47, 23))
+
+        // process
+        knotHash(lengths.toTypedArray(), string, 64)
+
+        // post-process
+        val hexes = string
+                .chunked(16)
+                .map { it.reduce { a, b -> a.xor(b) } }
+                .joinToString("") { String.format("%1$02X", it) }
+
+        // print
+        println(hexes)
+    }
 }
