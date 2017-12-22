7a702a331705d7ebac8fb964f42582636817d04a
Rowan Hill <rowan.hill@softwire.com>
2017-12-22 05:48:01 +0000

    Day 22: parts 1 & 2 in same file at same time

--- day22/src/app.kt
+++ day22/src/app.kt
@@ -7,14 +7,77 @@ enum class State {
     Flagged
 }
 
-fun main(args: Array<String>) {
-    val directions = listOf(
+class VirusCarrier {
+    var curPosition = Pair(0, 0)
+    private var curDirectionIndex = 0
+
+    private val directions = listOf(
             Pair(-1, 0), // up    = 0
             Pair(0, 1),  // right = 1
             Pair(1, 0),  // down  = 2
             Pair(0, -1)  // left  = 3
     )
 
+    fun turnLeft() {
+        curDirectionIndex = (curDirectionIndex - 1 + directions.size) % directions.size
+    }
+    fun turnRight() {
+        curDirectionIndex = (curDirectionIndex + 1) % directions.size
+    }
+    fun move() {
+        curPosition = Pair(
+                curPosition.first + directions[curDirectionIndex].first,
+                curPosition.second + directions[curDirectionIndex].second
+        )
+    }
+}
+
+fun part1() {
+    val mapSizeHalf = 12
+    val infected = HashSet<Pair<Int, Int>>()
+    File("src/input.txt").readLines().forEachIndexed { lineIndex, line ->
+        line.toCharArray().forEachIndexed { colIndex, c ->
+            if (c == '#') {
+                infected.add(Pair(lineIndex - mapSizeHalf, colIndex - mapSizeHalf))
+            }
+        }
+    }
+
+    val virusCarrier = VirusCarrier()
+
+    var numInfected = 0
+    fun isInfected(): Boolean {
+        return infected.contains(virusCarrier.curPosition)
+    }
+    fun infect() {
+        numInfected++
+        infected.add(virusCarrier.curPosition)
+    }
+    fun clean() {
+        infected.remove(virusCarrier.curPosition)
+    }
+
+    repeat(10000) {
+        if (isInfected()) {
+            virusCarrier.turnRight()
+        } else {
+            virusCarrier.turnLeft()
+        }
+
+        if (!isInfected()) {
+            infect()
+        } else {
+            clean()
+        }
+
+        virusCarrier.move()
+    }
+
+    println(numInfected)
+}
+
+
+fun part2() {
     val weakened = HashSet<Pair<Int, Int>>()
     val flagged = HashSet<Pair<Int, Int>>()
     val mapSizeHalf = 12
@@ -27,31 +90,17 @@ fun main(args: Array<String>) {
         }
     }
 
-    var curPosition = Pair(0, 0)
-    var curDirectionIndex = 0
-
-    fun turnLeft() {
-        curDirectionIndex = (curDirectionIndex - 1 + directions.size) % directions.size
-    }
-    fun turnRight() {
-        curDirectionIndex = (curDirectionIndex + 1) % directions.size
-    }
-    fun move() {
-        curPosition = Pair(
-                curPosition.first + directions[curDirectionIndex].first,
-                curPosition.second + directions[curDirectionIndex].second
-                )
-    }
+    val virusCarrier = VirusCarrier()
 
     var numInfected = 0
     fun isInfected(): Boolean {
-        return infected.contains(curPosition)
+        return infected.contains(virusCarrier.curPosition)
     }
     fun isWeakened(): Boolean {
-        return weakened.contains(curPosition)
+        return weakened.contains(virusCarrier.curPosition)
     }
     fun isFlagged(): Boolean {
-        return flagged.contains(curPosition)
+        return flagged.contains(virusCarrier.curPosition)
     }
     fun getState(): State {
         return when {
@@ -62,30 +111,30 @@ fun main(args: Array<String>) {
         }
     }
     fun weaken() {
-        weakened.add(curPosition)
+        weakened.add(virusCarrier.curPosition)
     }
     fun flag() {
-        infected.remove(curPosition)
-        flagged.add(curPosition)
+        infected.remove(virusCarrier.curPosition)
+        flagged.add(virusCarrier.curPosition)
     }
     fun infect() {
         numInfected++
-        weakened.remove(curPosition)
-        infected.add(curPosition)
+        weakened.remove(virusCarrier.curPosition)
+        infected.add(virusCarrier.curPosition)
     }
     fun clean() {
-        flagged.remove(curPosition)
-        infected.remove(curPosition)
+        flagged.remove(virusCarrier.curPosition)
+        infected.remove(virusCarrier.curPosition)
     }
 
     repeat(10000000) {
         val curState = getState()
-        
+
         when (curState) {
-            State.Clean -> turnLeft()
+            State.Clean -> virusCarrier.turnLeft()
             State.Weakened -> { /* no-op */ }
-            State.Infected -> turnRight()
-            State.Flagged -> { turnLeft(); turnLeft() }
+            State.Infected -> virusCarrier.turnRight()
+            State.Flagged -> { virusCarrier.turnLeft(); virusCarrier.turnLeft() }
         }
 
         when (curState) {
@@ -95,8 +144,13 @@ fun main(args: Array<String>) {
             State.Flagged -> clean()
         }
 
-        move()
+        virusCarrier.move()
     }
 
     println(numInfected)
+}
+
+fun main(args: Array<String>) {
+    part1()
+    part2()
 }
